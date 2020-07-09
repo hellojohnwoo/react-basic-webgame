8c81516f58d5f126e37bba2307b63caeb71d8f25
WOOYOUNG JEONG <hellojohnwoo@gmail.com>
2020-07-09 22:52:11 +0900

    0709 Applying WebPack to before project

---
+++ o1_multiplication_table/client.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+
+const MultipliWebPack = require('./o4_MultpliWebPack');
+
+ReactDom.render(<MultipliWebPack />, document.querySelector('#root'));
---
+++ o1_multiplication_table/o4_MultpliWebPack.jsx
@@ -0,0 +1,43 @@
+const React = require('react');
+const { useState, useRef } = React;
+
+const MulitpliWebPack = () => {       // Destructuring assignment
+    const [ firstNumber, setFirstNumber ] = useState(Math.ceil(Math.random() * 9));
+    const [ secondNumber, setSecondNumber ] = useState(Math.ceil(Math.random() * 9));
+    const [ value, setValue ] = useState('');
+    const [ result, setResult ] = useState('');
+    const inputEl = useRef(null);
+
+    const onSubmitForm  = (e) => {
+        e.preventDefault();
+        if (parseInt(value) === firstNumber * secondNumber) {
+            setResult('Right Answer :' + value);
+            setFirstNumber(Math.ceil(Math.random() * 9));
+            setSecondNumber(Math.ceil(Math.random() * 9));
+            setValue('');
+            inputEl.current.focus();
+        } else  {
+            setResult('Wrong Answer : ' + value);
+            setValue('');
+            inputEl.current.focus();
+        }
+    };
+
+    return (
+        <>
+            <div>{firstNumber} X {secondNumber} = ?</div>
+            <form onSubmit={onSubmitForm}>
+                <input
+                    ref={inputEl}
+                    type="number"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
+                <button>Input Number</button>
+            </form>
+            <div id="result">{result}</div>
+        </>
+    );
+}
+
+module.exports = MulitpliWebPack;
